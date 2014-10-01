2c306521fd3e1dda13b0e0110a0e5551888f66d0
Gabriel Loy <loy.gabriel@dev.redmintlabs.com>
2014-10-01 18:09:41 -0300

    Exercises

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,6 +4,7 @@ class UsersControllerTest < ActionController::TestCase
   test "should get new" do
     get :new
     assert_response :success
+    assert_select "title", "Sign up | Ruby on Rails Tutorial Sample App"
   end
 
 end

fbe4439caadd73ec519dc557689f30783ea287e6
manuel <manuelgambinob@gmail.com>
2014-10-01 21:24:11 -0300

    fixing merge conflicts

--- sample_app/config/initializers/backtrace_silencers.rb
+++ sample_app/config/initializers/backtrace_silencers.rb
@@ -1,6 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-<<<<<<< HEAD
 # You can add backtrace silencers for libraries that you're using but don't
 # wish to see in your backtraces.
 Rails.backtrace_cleaner.add_silencer { |line| line =~ /rvm/ }
@@ -8,10 +7,3 @@ Rails.backtrace_cleaner.add_silencer { |line| line =~ /rvm/ }
 # You can also remove all the silencers if you're trying to debug a problem
 # that might stem from framework code.
 # Rails.backtrace_cleaner.remove_silencers!
-=======
-# You can add backtrace silencers for libraries that you're using but don't wish to see in your backtraces.
-# Rails.backtrace_cleaner.add_silencer { |line| line =~ /my_noisy_library/ }
-Rails.backtrace_cleaner.add_silencer { |line| line =~ /rvm/ }
-# You can also remove all the silencers if you're trying to debug a problem that might stem from framework code.
-# Rails.backtrace_cleaner.remove_silencers!
->>>>>>> 46d184206a09665eeda2ac631bb3f4c1c8fc2a75

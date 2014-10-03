ed38a0089edb1c488afd089500fbb75efbe072f7
manugb <manuelgambinob@gmail.com>
2014-10-03 15:51:50 -0300

    Parte del cap 8

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  resources :users
-  get 'users/new'
 
   root             'static_pages#home'
-  get 'help'    => 'static_pages#help'
-  get 'about'   => 'static_pages#about'
-  get 'contact' => 'static_pages#contact'
-  get 'signup'  =>  'users#new'
+  get 	 'help'    => 'static_pages#help'
+  get 	 'about'   => 'static_pages#about'
+  get 	 'contact' => 'static_pages#contact'
+  get 	 'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+  resources :users
 end

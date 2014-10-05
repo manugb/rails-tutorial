3a7d6a8bcf5fa4e27b0e669400b919e8c362af08
manuel <manuelgambinob@gmail.com>
2014-10-05 19:44:14 -0300

    HelloApp y toyApp elminadas para probar deploy heroku

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ActiveRecord::Base
+	before_save { email.downcase! }
+
+	validates :name, presence: true, length:{maximum: 50}
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+	validates :email, presence: true, format: {with: VALID_EMAIL_REGEX},
+						uniqueness: {case_sensitive: false}
+	validates :password, length: {minimum: 6}
+	has_secure_password
+
+	# Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+end
